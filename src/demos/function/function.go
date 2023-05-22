38801118c470cd9088b07a7010b3bc16631203fa
MarkXE <mark.li@xenera.jp>
2023-05-22 10:34:00 +0800

    add some new notes for function naming rule

--- src/demos/function/function.go
+++ src/demos/function/function.go
@@ -2,6 +2,12 @@ package fun
 
 import "fmt"
 
+/**
+  方法命名：
+	1. 首字母大写，为包向外暴露的方法，供其他包程序调用
+	2. 首字母小写，为包内私有方法，只供同一个包内程序调用
+*/
+
 // 返回多个匿名返回值
 func MultipleAnonymousReturn(p1 string, p2 int) (int, string) {
 	fmt.Println("MultipleAnonymousReturn called...")
