df11c3d128c6ab96ed08b8d9d99901cd293965df
MarkXE <mark.li@xenera.jp>
2023-05-24 16:08:35 +0800

    add new demonstrations for channel

--- src/demos/goroutine/channel.go
+++ src/demos/goroutine/channel.go
@@ -1,6 +1,9 @@
 package gr
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 /*
 	channel:
@@ -12,10 +15,17 @@ import "fmt"
 		6. channel的基本操作：
 			6.1 发送： ch <- 数据
 			6.2 接收： x := <- ch
-			6.3 关闭： close(ch)
+			6.3 关闭： close(ch)，如果不在向channel内写入数据或从channel中读取数据，关闭channel
+				6.3.1 读取数据goroutine判断channel是否关闭的方式： (data,ok := <- ch, ok为false时，表示channel以关闭)，(使用for range方式)
+		7. 无缓冲channel：又称为阻塞channel，当没有接收者时，发送操作将阻塞；当没有发送者时，接收操作将阻塞
+		8. 有缓冲channel：当缓冲区满时，发送操作将阻塞；当缓冲区空时，接收操作将阻塞
+		9. 单向channel，
+			9.1 写channel：chan<- 元素类型
+			9.2 读channel：<-chan 元素类型
+			9.3 双向channel类型变量可以给单向channel类型变量赋值，反之则不可以
 */
 
-func TestChannel() {
+func testNobufferChannel() {
 	// 创建一个channel
 	cha := make(chan int)
 
@@ -30,4 +40,98 @@ func TestChannel() {
 	// main coroutine从channel中获取数据
 	data := <-cha
 	fmt.Println("main coroutine get data -->", data)
+}
+
+func testBufferChannel(bufferCap int) {
+	// 创建有缓冲channel，缓冲区大小为3
+	cha := make(chan int, bufferCap)
+
+	// 打印channel buffer的长度和容量
+	fmt.Println("before len(cha) -->", len(cha), ", cap(cha) -->", cap(cha))
+
+	// 创建goroutine
+	go func() {
+		defer fmt.Println("sub goroutine end...")
+		// 循环向channel中发送数据
+		for i := 0; i < bufferCap; i++ {
+			cha <- i
+			fmt.Println("len(cha) -->", len(cha), "cap(cha) -->", cap(cha))
+		}
+	}()
+
+	// main goroutine睡两秒，保证子goroutine执行完毕
+	time.Sleep(2 * time.Second)
+
+	// main goroutine从channel中获取数据
+	for i := 0; i < bufferCap; i++ {
+		num := <-cha
+		fmt.Println("num =", num)
+	}
+
+	fmt.Println("main goroutine end ...")
+}
+
+func testCloseChannel() {
+	cha := make(chan int)
+	go func() {
+		for i := 0; i < 4; i++ {
+			cha <- i
+		}
+		// 写数据完成，关闭channel
+		close(cha)
+	}()
+	// 循环取数据
+	for {
+		// 持有初始化语句的if，可定义代码块局部变量
+		if data, ok := <-cha; ok {
+			fmt.Println("data -->", data)
+		} else {
+			// channel关闭，退出循环
+			break
+		}
+	}
+
+}
+
+// cha 写channel
+func writeData(cha chan<- int) {
+	// 向写channel中写入数据
+	defer fmt.Println("写数据结束...")
+	for i := 0; i < 4; i++ {
+		cha <- i
+	}
+	close(cha)
+}
+
+// cha 读channel
+func readData(cha <-chan int) {
+	for data := range cha {
+		fmt.Println("data -->", data)
+	}
+}
+
+func testUnidirectionalChannel() {
+	// 定义双向channel
+	cha := make(chan int)
+	// 写数据
+	go writeData(cha)
+	// 读数据
+	go readData(cha)
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestChannel() {
+	fmt.Println("--------no buffer channel---------")
+	testNobufferChannel()
+
+	fmt.Println("--------buffer channel---------")
+	bufferCap := 3
+	testBufferChannel(bufferCap)
+
+	fmt.Println("--------closed channel---------")
+	testCloseChannel()
+
+	fmt.Println("--------unidirectional channel---------")
+	testUnidirectionalChannel()
 }
