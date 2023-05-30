b4e16e181ea4ad01e0901be021d7864cd2ef9b0d
MarkXE <mark.li@xenera.jp>
2023-05-30 16:02:48 +0800

    add demos for sync

--- src/demos/variable/variablesDeclaration.go
+++ src/demos/variable/variablesDeclaration.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+/*
+	golang变量类型
+		1. 值类型：int, float, bool, string, struct, array等，在做等号赋值操作或函数传参时，进行拷贝复制，值传递
+		2. 引用类型：slice, map, channel, interface, 等号赋值或函数传参时，传递的是指向值的指针
+		3. make和new：make是初始化内置的数据结构(slice, map, channel)，new是根据传入的类型，分配一片内存空间并返回指向这片内存空间的指针
+*/
+
 func VariableDeclaration() {
 	// 1. 声明变量，按变量类型取默认值
 	var str1 string
@@ -30,4 +37,13 @@ func VariableDeclaration() {
 		bool1 = true
 	)
 	fmt.Println("int1 =", int1, "bool1 =", bool1)
+
+	// make
+	slice := make([]int, 2, 3)
+	fmt.Println("slice value: ", slice)
+
+	// new
+	p := new(int)
+	*p = 3
+	fmt.Println("p value: ", p, ", *p value: ", *p)
 }
