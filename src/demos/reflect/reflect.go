82abf511f5b1e29b78c7cd3c6b740d33942b9784
MarkXE <mark.lee21077@gmail.com>
2023-12-07 10:37:04 +0800

    modify reflect demo

--- src/demos/reflect/reflect.go
+++ src/demos/reflect/reflect.go
@@ -16,6 +16,10 @@ func (this Student) Study() {
 	fmt.Println(this.name, "is studying...")
 }
 
+func (this Student) sleep() {
+	fmt.Println(this.name, "is sleeping...")
+}
+
 func getFieldAndMethod(arg interface{}) {
 	// 获取参数类型
 	argType := reflect.TypeOf(arg)
@@ -32,7 +36,9 @@ func getFieldAndMethod(arg interface{}) {
 		fmt.Println("field name", field.Name, ", field value", fieldValue)
 	}
 
-	// 获取所有method TODO  NumMethod方法的返回值
+	// 获取所有method
+	// 非接口类型，返回公有方法的数量
+	// 接口类型，返回公有和私有方法的数量总和
 	for i := 0; i < argType.NumMethod(); i++ {
 		method := argType.Method(i)
 		fmt.Println("method name", method.Name, ", is exported", method.IsExported())
